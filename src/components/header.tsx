796bb37103e3d8699fa9b5ba25846f8f6a63c8d1
nevermature <nevermature01@gmail.com>
2026-01-30 11:54:21 +0000

    give card glassy look (_for element <SheetHeader>_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -75,7 +75,7 @@ export function Header() {
               </SheetTrigger>
               <SheetContent
                 side="left"
-                className="inset-y-auto left-4 top-20 h-auto w-auto max-w-[300px] rounded-2xl bg-background/70 p-0 backdrop-blur-xl border border-white/20 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:slide-out-to-top-5 data-[state=open]:slide-in-from-top-5"
+                className="inset-y-auto left-4 top-20 h-auto w-auto max-w-[300px] rounded-2xl bg-background/80 p-0 backdrop-blur-2xl border border-white/20 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:slide-out-to-top-5 data-[state=open]:slide-in-from-top-5"
               >
                 <div className="flex flex-col">
                     <SheetHeader className="p-6 pb-4 pr-12">

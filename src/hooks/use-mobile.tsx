50d41c3d2443299bd16c1be6405b5320ccb50b76
nevermature <nevermature01@gmail.com>
2026-01-30 10:10:50 +0000

    phone mein images load NAHI ho rahi

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,17 +3,26 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+
+    // Set the initial value on the client
+    setIsMobile(mql.matches)
+
+    const handleResize = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    // Listen for changes
+    mql.addEventListener("change", handleResize)
+
+    // Cleanup
+    return () => {
+      mql.removeEventListener("change", handleResize)
+    }
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
